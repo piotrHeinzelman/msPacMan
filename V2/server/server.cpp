0f83ca2ce9ebdec607085129e1fb8a12181da7c7
Piotr Heinzeman <piotr@i-zn.pl>
2023-07-29 15:19:24 +0200

    zad1

--- V2/server/server.cpp
+++ V2/server/server.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 
-int main(){
-printf("?");
-//   std::cout << "abc!";
-   return 0;
+class Eye{
+   private:
+   std::string _name;
 
+   public:
+   Eye( std::string name="blue"  ){ _name = name; }
+
+
+   std::string getName(){ return "oczko " + _name + "\r\n"; };
+
+   void setName( std::string name="blue" ){
+//      _name = name;
+      this->_name = name;
+      (*this)._name = name;
+   }
+};
+
+
+class Bear{
+   public:
+   Eye lewe;
+   Eye prawe;
+
+};
+
+
+int main(){
+   Bear bear = Bear();
+   bear.lewe = Eye();
+   bear.prawe = Eye("red");
+   std::cout << bear.lewe.getName();
+   std::cout << bear.prawe.getName();
+
+
+
+   Bear* Wbear = new Bear();
+   std::cout << Wbear->lewe.getName();
+   std::cout << Wbear->prawe.getName();
+
+
+   return 0;
 }
