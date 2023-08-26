65753b3f4ca527411f869db0f50e2f5643978538
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-08-26 20:44:58 +0200

    ADD :: thread

--- main.cpp
+++ main.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <process.h>
 //#include "info/con_serv.h"
 
 #include "allTest.h"
+#include "src/test.h"
+
+
+
+
+DWORD WINAPI mythreadA( LPVOID lpParameter )
+{
+    printf("CreateThread %d \n", GetCurrentThreadId());
+    return 0;
+}
+
+unsigned int __stdcall mythreadB(void* data)
+{
+    printf("_beginthreadex %d \n", GetCurrentThreadId());
+    return 0;
+}
+
+void mythreadC(void* data)
+{
+    printf("_beginthread %d \n", GetCurrentThreadId());
+}
+
 
 int main() {
 
+    HANDLE myhandleA, myhandleB, myhandleC;
+
+    myhandleA = CreateThread(0, 0, mythreadA, 0, 0, 0);
+
+    myhandleB = (HANDLE)_beginthreadex(0, 0, &mythreadB, 0, 0, 0);
+    WaitForSingleObject(myhandleB, INFINITE);
 
 
+    myhandleC = (HANDLE)_beginthread(&mythreadC, 0, 0);
+    getchar();
+
+    return 0;
 
 
     // run test
---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by John on 2023-08-26.
+//
+
+#include "test.h"
