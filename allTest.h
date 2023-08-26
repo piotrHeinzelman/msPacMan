caddc206fdf2c4812147e5edc2eecc71d0b08650
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-08-26 21:16:26 +0200

    ADD :: test in thread !

--- allTest.h
+++ allTest.h
@@ -9,12 +9,30 @@
 
 #include "tests/ServTest.h"
 
+
+DWORD WINAPI runTestInThread( LPVOID lpParameter ) {
+    printf("CreateThread %d \n", GetCurrentThreadId());
+    printf("CreateThread Server in Thread \n");
+
+    TCPServ ts;               // domyslny port
+    ts.rec(); // odbior danych przez UDP
+
+    return 0;
+}
+
+
 void runMyTests() {
     printf ("[ TEST ] :: runAllTest();\n");
 
+    HANDLE myhandleA;
+    myhandleA = CreateThread(0, 0, runTestInThread, 0, 0, 0);
+
     printf ("[ TEST ] :: ServTest::runAllTest();\n");
-    ServTest servTest;
-    servTest.runAllTest();
+    //ServTest servTest;
+    //servTest.runAllTest();
+    Cli c;
+
+
 
     }
 
--- main.cpp
+++ main.cpp
@@ -13,38 +13,11 @@
 
 
 
-DWORD WINAPI mythreadA( LPVOID lpParameter )
-{
-    printf("CreateThread %d \n", GetCurrentThreadId());
-    return 0;
-}
 
-unsigned int __stdcall mythreadB(void* data)
-{
-    printf("_beginthreadex %d \n", GetCurrentThreadId());
-    return 0;
-}
-
-void mythreadC(void* data)
-{
-    printf("_beginthread %d \n", GetCurrentThreadId());
-}
 
 
 int main() {
 
-    HANDLE myhandleA, myhandleB, myhandleC;
-
-    myhandleA = CreateThread(0, 0, mythreadA, 0, 0, 0);
-
-    myhandleB = (HANDLE)_beginthreadex(0, 0, &mythreadB, 0, 0, 0);
-    WaitForSingleObject(myhandleB, INFINITE);
-
-
-    myhandleC = (HANDLE)_beginthread(&mythreadC, 0, 0);
-    getchar();
-
-    return 0;
 
 
     // run test
