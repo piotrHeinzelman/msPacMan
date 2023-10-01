5c22da189fa9889baa2880064a4a5166c80a9c87
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-01 17:49:19 +0200

    ..

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-"C:\CLion 2023.2\bin\cmake\win\x64\bin\cmake.exe" --build C:\Users\Bill\CLionProjects\WinBGIm-64\outFolder --target outFile -j 4
+REM  "C:\CLion 2023.2\bin\cmake\win\x64\bin\cmake.exe" --build C:\Users\Bill\CLionProjects\WinBGIm-64\outFolder --target outFile -j 4
+E:\CLion\CLion-2023.2.win\bin\cmake\win\x64\bin\cmake.exe --build E:\CLion\CLionProjects\msPacMan\cmake-build-debug --target msPacMan -j 3
+
