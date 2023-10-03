1c34bdc9ac68483decbe6a455b9cdd772393da6c
piotrHeinzelman <55515688+piotrHeinzelman@users.noreply.github.com>
2023-10-03 13:12:41 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+cmake  -G "MinGW Makefiles" -DCMAKE_C_COMPILER="C:/mingw64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="C:/mingw64/bin/g++.exe" -B build   %1 %2 %3 %4 %5 %6
+
 REM  "C:\CLion 2023.2\bin\cmake\win\x64\bin\cmake.exe" --build C:\Users\Bill\CLionProjects\WinBGIm-64\outFolder --target outFile -j 4
-E:\CLion\CLion-2023.2.win\bin\cmake\win\x64\bin\cmake.exe --build E:\CLion\CLionProjects\msPacMan\cmake-build-debug --target msPacMan -j 3
+REM E:\CLion\CLion-2023.2.win\bin\cmake\win\x64\bin\cmake.exe --build E:\CLion\CLionProjects\msPacMan\cmake-build-debug --target msPacMan -j 3
 
